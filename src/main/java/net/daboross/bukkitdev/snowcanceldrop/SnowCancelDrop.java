dd759c7fd6c60e9eed009af2bbab0349c5711aa4
Dabo Ross <dabodialga@gmail.com>
2013-07-22 23:03:24 -0700

    Fixed durability again

--- src/main/java/net/daboross/bukkitdev/snowcanceldrop/SnowCancelDrop.java
+++ src/main/java/net/daboross/bukkitdev/snowcanceldrop/SnowCancelDrop.java
@@ -55,7 +55,11 @@ public class SnowCancelDrop extends JavaPlugin implements Listener {
 			evt.setCancelled(true);
 			Location location = evt.getBlock().getLocation();
 			location.getWorld().dropItem(location, new ItemStack(Material.SNOW_BLOCK));
-			evt.getPlayer().getItemInHand().setDurability((short) (evt.getPlayer().getItemInHand().getDurability() + 1));
+			ItemStack hand = evt.getPlayer().getItemInHand();
+			hand.setDurability((short) (hand.getDurability() + 1));
+			if (hand.getDurability() > hand.getType().getMaxDurability()) {
+				hand.setType(Material.AIR);
+			}
 			evt.getBlock().setType(Material.AIR);
 		}
 	}
